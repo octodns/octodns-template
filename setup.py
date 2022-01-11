ea489a32785eb94a6e0766fa884a44e545823ca3
Ross McFarland <rwmcfa1@gmail.com>
2022-01-11 08:01:58 -0800

    Use find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 def descriptions():
@@ -25,7 +25,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     name='{MODULE_DASHED}',
-    packages=('{MODULE}',),
+    packages=find_packages(),
     python_requires='>=3.6',
     install_requires=('octodns>=0.9.14', 'TODO: other requirements'),
     url='https://github.com/octodns/{MODULE_DASHED}',
